de73afc983d2ffd39494108895e74d3222bca721
D-Perm <danieldkdao@gmail.com>
2025-04-17 21:49:22 -0700

    Update guitarScales.js

--- guitarScales.js
+++ guitarScales.js
@@ -44,154 +44,154 @@ button.onclick = function (){
     }
     else if(type.value === "OneStringA"){
         if (scale.value === "C"){
-            div.innerHTML = `<img src="CScale-A.png">`;
+            div.innerHTML = `<img src="CScale-A.png">` + `<img src="15Frets.png">`;
         }
         else if(scale.value === "D"){
-            div.innerHTML = `<img src="DScale-A.png">`;
+            div.innerHTML = `<img src="DScale-A.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "E"){
-            div.innerHTML = `<img src="EScale-A.png">`;
+            div.innerHTML = `<img src="EScale-A.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "F"){
-            div.innerHTML = `<img src="FScale-A.png">`;
+            div.innerHTML = `<img src="FScale-A.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "G"){
-            div.innerHTML = `<img src="GScale-A.png">`;
+            div.innerHTML = `<img src="GScale-A.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "A"){
-            div.innerHTML = `<img src="AScale-A.png">`;
+            div.innerHTML = `<img src="AScale-A.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "B"){
-            div.innerHTML = `<img src="BScale-A.png">`;
+            div.innerHTML = `<img src="BScale-A.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "CD"){
-            div.innerHTML = `<img src="CDScale-A.png">`;
+            div.innerHTML = `<img src="CDScale-A.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "DE"){
-            div.innerHTML = `<img src="DEScale-A.png">`;
+            div.innerHTML = `<img src="DEScale-A.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "FG"){
-            div.innerHTML = `<img src="FGScale-A.png">`;
+            div.innerHTML = `<img src="FGScale-A.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "GA"){
-            div.innerHTML = `<img src="GAScale-A.png">`;
+            div.innerHTML = `<img src="GAScale-A.png">` + `<img src="16Frets.png">`;
         }
         else {
-            div.innerHTML = `<img src="ABScale-A.png">`;
+            div.innerHTML = `<img src="ABScale-A.png">` + `<img src="16Frets.png">`;
         }
     }
     else if(type.value === "OneStringD"){
         if (scale.value === "C"){
-            div.innerHTML = `<img src="CScale-D.png">`;
+            div.innerHTML = `<img src="CScale-D.png">` + `<img src="15Frets.png">`;
         }
         else if(scale.value === "D"){
-            div.innerHTML = `<img src="DScale-D.png">`;
+            div.innerHTML = `<img src="DScale-D.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "E"){
-            div.innerHTML = `<img src="EScale-D.png">`;
+            div.innerHTML = `<img src="EScale-D.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "F"){
-            div.innerHTML = `<img src="FScale-D.png">`;
+            div.innerHTML = `<img src="FScale-D.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "G"){
-            div.innerHTML = `<img src="GScale-D.png">`;
+            div.innerHTML = `<img src="GScale-D.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "A"){
-            div.innerHTML = `<img src="AScale-D.png">`;
+            div.innerHTML = `<img src="AScale-D.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "B"){
-            div.innerHTML = `<img src="BScale-D.png">`;
+            div.innerHTML = `<img src="BScale-D.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "CD"){
-            div.innerHTML = `<img src="CDScale-D.png">`;
+            div.innerHTML = `<img src="CDScale-D.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "DE"){
-            div.innerHTML = `<img src="DEScale-D.png">`;
+            div.innerHTML = `<img src="DEScale-D.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "FG"){
-            div.innerHTML = `<img src="FGScale-D.png">`;
+            div.innerHTML = `<img src="FGScale-D.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "GA"){
-            div.innerHTML = `<img src="GAScale-D.png">`;
+            div.innerHTML = `<img src="GAScale-D.png">` + `<img src="16Frets.png">`;
         }
         else {
-            div.innerHTML = `<img src="ABScale-D.png">`;
+            div.innerHTML = `<img src="ABScale-D.png">` + `<img src="16Frets.png">`;
         }
     }
     else if(type.value === "OneStringG"){
         if (scale.value === "C"){
-            div.innerHTML = `<img src="CScale-G.png">`;
+            div.innerHTML = `<img src="CScale-G.png">` + `<img src="15Frets.png">`;
         }
         else if(scale.value === "D"){
-            div.innerHTML = `<img src="DScale-G.png">`;
+            div.innerHTML = `<img src="DScale-G.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "E"){
-            div.innerHTML = `<img src="EScale-G.png">`;
+            div.innerHTML = `<img src="EScale-G.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "F"){
-            div.innerHTML = `<img src="FScale-G.png">`;
+            div.innerHTML = `<img src="FScale-G.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "G"){
-            div.innerHTML = `<img src="GScale-G.png">`;
+            div.innerHTML = `<img src="GScale-G.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "A"){
-            div.innerHTML = `<img src="AScale-G.png">`;
+            div.innerHTML = `<img src="AScale-G.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "B"){
-            div.innerHTML = `<img src="BScale-G.png">`;
+            div.innerHTML = `<img src="BScale-G.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "CD"){
-            div.innerHTML = `<img src="CDScale-G.png">`;
+            div.innerHTML = `<img src="CDScale-G.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "DE"){
-            div.innerHTML = `<img src="DEScale-G.png">`;
+            div.innerHTML = `<img src="DEScale-G.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "FG"){
-            div.innerHTML = `<img src="FGScale-G.png">`;
+            div.innerHTML = `<img src="FGScale-G.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "GA"){
-            div.innerHTML = `<img src="GAScale-G.png">`;
+            div.innerHTML = `<img src="GAScale-G.png">` + `<img src="16Frets.png">`;
         }
         else {
-            div.innerHTML = `<img src="ABScale-G.png">`;
+            div.innerHTML = `<img src="ABScale-G.png">` + `<img src="16Frets.png">`;
         }
     }
     else if(type.value === "OneStringB"){
         if (scale.value === "C"){
-            div.innerHTML = `<img src="CScale-B.png">`;
+            div.innerHTML = `<img src="CScale-B.png">` + `<img src="15Frets.png">`;
         }
         else if(scale.value === "D"){
-            div.innerHTML = `<img src="DScale-B.png">`;
+            div.innerHTML = `<img src="DScale-B.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "E"){
-            div.innerHTML = `<img src="EScale-B.png">`;
+            div.innerHTML = `<img src="EScale-B.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "F"){
-            div.innerHTML = `<img src="FScale-B.png">`;
+            div.innerHTML = `<img src="FScale-B.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "G"){
-            div.innerHTML = `<img src="GScale-B.png">`;
+            div.innerHTML = `<img src="GScale-B.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "A"){
-            div.innerHTML = `<img src="AScale-B.png">`;
+            div.innerHTML = `<img src="AScale-B.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "B"){
-            div.innerHTML = `<img src="BScale-B.png">`;
+            div.innerHTML = `<img src="BScale-B.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "CD"){
-            div.innerHTML = `<img src="CDScale-B.png">`;
+            div.innerHTML = `<img src="CDScale-B.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "DE"){
-            div.innerHTML = `<img src="DEScale-B.png">`;
+            div.innerHTML = `<img src="DEScale-B.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "FG"){
-            div.innerHTML = `<img src="FGScale-B.png">`;
+            div.innerHTML = `<img src="FGScale-B.png">` + `<img src="16Frets.png">`;
         }
         else if(scale.value === "GA"){
-            div.innerHTML = `<img src="GAScale-B.png">`;
+            div.innerHTML = `<img src="GAScale-B.png">` + `<img src="16Frets.png">`;
         }
         else {
-            div.innerHTML = `<img src="ABScale-B.png">`;
+            div.innerHTML = `<img src="ABScale-B.png">` + `<img src="16Frets.png">`;
         }
     }
     else{
